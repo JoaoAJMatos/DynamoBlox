ce9c36e023913bb1a2686121e16688c7a3db513b
Joao Matos <joaoafonsoj2004@gmail.com>
2021-12-29 01:50:49 +0000

    Keccak Hashing implementation

--- util/index.js
+++ util/index.js
@@ -1,9 +1,19 @@
 // DynamoBlox Utility
+const keccak256 = require('js-sha3').keccak256;
 
 const sortCharacters = data => {
       return JSON.stringify(data).split('').sort().join(''); // Produces the same output string if the order of the elements in an object is changed
 }
 
+const keccakHash = data => { // Hash incomming string
+      const hash = keccak256.create();
+
+      hash.update(sortCharacters(data));
+
+      return hash.hex();
+}
+
 module.exports = {
-      sortCharacters
+      sortCharacters,
+      keccakHash
 };
--- util/tests/index.test.js
+++ util/tests/index.test.js
@@ -1,4 +1,5 @@
-const { sortCharacters } = require('../index');
+const { describe, it } = require('@jest/globals');
+const { sortCharacters, keccakHash } = require('../index');
 
 describe('util', () => {
       describe('sortCharacters()', () => {
@@ -10,4 +11,11 @@ describe('util', () => {
                   expect(sortCharacters({ dynamo: 'dynamo' })).not.toEqual(sortCharacters({ blox: 'blox' }));
             });
       });
+
+      describe('keccakHash()', () => {
+            it('produces a keccak256 hash', () =>  {
+                  expect(keccakHash('dynamoBlox')).toEqual('e0b92a1e8fbeff81ad85755fe3573346620e39f326568020d501bc9cf68f7d10');
+            });
+      });
 });
+
