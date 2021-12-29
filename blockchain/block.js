c5e696af6b7e9f2d81f5cdb2b66469d520e107a6
Joao Matos <joaoafonsoj2004@gmail.com>
2021-12-29 12:21:43 +0000

    Block Target Hash and Tests

--- blockchain/block.js
+++ blockchain/block.js
@@ -1,17 +1,30 @@
 const { GENESIS_DATA } = require('../config');
+
+const HASH_LENGTH = 64;
+const MAX_HASH_VALUE = parseInt("F".repeat(HASH_LENGTH), 16);
+
+class Block {
+    constructor({ blockHeaders }) {
+        this.blockHeaders = blockHeaders;
+    }
+
+    static calculateBlockTargetHash({ lastBlock }) {
+        const value = (MAX_HASH_VALUE / lastBlock.blockHeaders.difficulty).toString(16);
+    
+        if (value.length > HASH_LENGTH) {
+            return "F".repeat(HASH_LENGTH);
+        }
+
+        return "0".repeat(HASH_LENGTH - value.length) + value;
+    }
   
-  class Block {
-      constructor({ blockHeaders }) {
-          this.blockHeaders = blockHeaders;
-      }
+    static mineBlock({ lastBlock, beneficiary }) {
+        
+    }
   
-      static mineBlock({ lastBlock }) {
-  
-      }
-  
-      static genesis() { // Returns the GENESIS Block
-          return new this(GENESIS_DATA);
-      }
-  }
-  
-  module.exports = Block;
+    static genesis() { // Returns the GENESIS Block
+        return new this(GENESIS_DATA);
+    }
+}
+
+module.exports = Block;
