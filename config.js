2b99d2967d43b46086b0edf0712cfa3875a1962e
Joao Matos <joaoafonsoj2004@gmail.com>
2021-12-29 01:21:38 +0000

    Blockchain, Blocks, and the GENESIS block

---
+++ config.js
@@ -0,0 +1,14 @@
+const GENESIS_DATA = {
+          blockHeaders: {
+                    parentHash: "DYNAMOBLOX",
+                    beneficiary: "DYNAMOBLOX",
+                    difficulty: 1,
+                    depth: 0,
+                    timestamp: "DYNAMO-EPOCH",
+                    nonce: 0
+          }
+};
+
+module.exports = {
+          GENESIS_DATA
+};
