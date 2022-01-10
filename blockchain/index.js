0fcea7e2afb7d3ea4316ec6251e8259c2f8c0120
Joao Matos <joaoafonsoj2004@gmail.com>
2022-01-10 17:57:42 +0000

    local API

--- blockchain/index.js
+++ blockchain/index.js
@@ -20,24 +20,13 @@ class Blockchain {
 
                 this.chain.push(block);
                 this.blockchainDB.write(this.chain); // Store the block
+                this.chain = this.blockchainDB.read();
 
                 return resolve();
             
-            }).catch(error => reject(error)); // If the validation is unsuccessful, return error
+            }).catch(reject); // If the validation is unsuccessful, return error
         });
     }
 }
 
 module.exports = Blockchain;
-
-// Test
-blockchain = new Blockchain();
-console.log('Chain: ', blockchain.chain)
-let block = Block.mineBlock({ lastBlock: blockchain.chain[blockchain.chain.length - 1], beneficiary: "dynamo4" });
-
-blockchain.addBlock({ block }).then(() => {
-    console.log("Final Chain => ", blockchain.chain);
-
-}).catch(error => {
-    console.error(error)
-});
